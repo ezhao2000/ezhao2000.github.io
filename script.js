d9d326f68a92629ef73c5a2ec2ddaa28a4e5fa78
ezhao2000 <140916621+ezhao2000@users.noreply.github.com>
2023-07-30 12:58:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Fetch the data from the CSV file
+
 d3.csv("data/data.csv").then(function (data) {
     // Parse the dates and prices
     const parseDate = d3.timeParse("%Y-%m");
@@ -7,7 +7,7 @@ d3.csv("data/data.csv").then(function (data) {
       d.Price = +d.Price;
     });
   
-    // Set up the SVG element and scales
+
     const margin = { top: 50, right: 50, bottom: 70, left: 70 };
     const width = 850 - margin.left - margin.right;
     const height = 500 - margin.top - margin.bottom;
@@ -22,13 +22,13 @@ d3.csv("data/data.csv").then(function (data) {
     const xScale = d3.scaleTime().range([0, width]);
     const yScale = d3.scaleLinear().range([height, 0]);
   
-    // Line generator
+    
     const line = d3
       .line()
       .x((d) => xScale(d.Month))
       .y((d) => yScale(d.Price));
   
-    // Append the line path
+ 
     const linePath = svg
       .append("path")
       .attr("class", "line")
@@ -36,12 +36,12 @@ d3.csv("data/data.csv").then(function (data) {
       .attr("stroke", "steelblue")
       .attr("stroke-width", 2);
   
-    // Append the axes
+ 
     svg.append("g").attr("class", "x-axis").attr("transform", "translate(0," + height + ")");
   
     svg.append("g").attr("class", "y-axis");
   
-    // Add x-axis label
+    
     svg.append("text")
       .attr("class", "x-axis-label")
       .attr("x", width / 2)
@@ -49,7 +49,7 @@ d3.csv("data/data.csv").then(function (data) {
       .style("text-anchor", "middle")
       .text("Year");
   
-    // Add y-axis label
+  
     svg.append("text")
       .attr("class", "y-axis-label")
       .attr("x", -height / 2)
@@ -68,10 +68,10 @@ d3.csv("data/data.csv").then(function (data) {
             date: new Date("2001-01"),
             value: 9.3,
             text: "Low supply and high demand due to unusually cold weather",
-            xOffset: -10, // Custom X offset for annotation
-            yOffset: 0, // Custom Y offset for annotation
+            xOffset: -10, 
+            yOffset: 0, 
             showCircle: true,
-            circleX: -6, // Custom X-position for the circle
+            circleX: -6, 
             circleY: 12,
             
           },
@@ -79,13 +79,13 @@ d3.csv("data/data.csv").then(function (data) {
             date: new Date("2003-10"),
             value: 12.42,
             text: "Hurricane Katrina and Rita disrupt supply from Gulf of Mexico",
-            xOffset: -45, // Custom X offset for annotation
-            yOffset: -30, // Custom Y offset for annotation
+            xOffset: -45,
+            yOffset: -30, 
             showCircle: true,
-            circleX: 147, // Custom X-position for the circle
+            circleX: 147, 
             circleY: -28,
           },
-          // Add any other annotations specific to the first tab here
+          
         ],
       },
       {
@@ -95,13 +95,13 @@ d3.csv("data/data.csv").then(function (data) {
             date: new Date("2011-01"),
             value: 13,
             text: "Hurricane Gustav and Ike disrupt supply from Gulf of Mexico",
-            xOffset: -22, // Custom X offset for annotation
-            yOffset: 13, // Custom Y offset for annotation
+            xOffset: -22,
+            yOffset: 13, 
             showCircle: true,
-            circleX: -212, // Custom X-position for the circle
+            circleX: -212, 
             circleY: 9,
           },
-          // Add any other annotations specific to the second tab here
+          
         ],
       },
       {
@@ -111,13 +111,13 @@ d3.csv("data/data.csv").then(function (data) {
             date: new Date("2020-01"),
             value: 7.5,
             text: "Russia Invades Ukraine causing volatility in global natural gas markets",
-            xOffset: 60, // Custom X offset for annotation
-            yOffset: -60, // Custom Y offset for annotation
+            xOffset: 60, 
+            yOffset: -60, 
             showCircle: true,
-            circleX: 273, // Custom X-position for the circle
+            circleX: 273,
             circleY: -57,
           },
-          // Add any other annotations specific to the third tab here
+    
         ],
       },
     ];
@@ -136,7 +136,7 @@ d3.csv("data/data.csv").then(function (data) {
         const xValue = xScale(annotation.date);
         const yValue = yScale(annotation.value);
   
-        // Add the annotation text above the lines with custom offsets
+     
         svg
           .append("text")
           .attr("class", "annotation-text")
@@ -146,7 +146,7 @@ d3.csv("data/data.csv").then(function (data) {
           .text(annotation.text);
   
        
-      // Draw the circle if showCircle is true for this annotation
+
       if (annotation.showCircle) {
         svg
           .append("circle")
@@ -159,10 +159,10 @@ d3.csv("data/data.csv").then(function (data) {
           .attr("stroke-width", 2);
       }
 
-    // Remove any existing footnote
+
   svg.selectAll(".footnote").remove();
 
-  // Add the footnote text at the bottom of the plot
+
   svg
     .append("text")
     .attr("class", "footnote")
@@ -176,7 +176,7 @@ d3.csv("data/data.csv").then(function (data) {
       });
     }
 
-    // Function to get the appropriate footnote text based on the selected tab
+  
 function getFootnoteText(tab) {
     switch (tab) {
       case "firstTab":
@@ -191,20 +191,20 @@ function getFootnoteText(tab) {
   }
   
     function updateLineplot(filteredData) {
-      // Update scales domain based on the filtered data
+
       xScale.domain(d3.extent(filteredData.data, (d) => d.Month));
       yScale.domain([0, d3.max(filteredData.data, (d) => d.Price)]);
   
-      // Update the line path
+   
       linePath.datum(filteredData.data).attr("d", line);
   
-      // Select and join data for circles (points)
+     
       const circles = svg.selectAll("circle").data(filteredData.data);
   
-      // Remove old data points
+     
       circles.exit().remove();
   
-      // Add new data points
+     
       circles
         .enter()
         .append("circle")
@@ -213,18 +213,18 @@ function getFootnoteText(tab) {
         .duration(500)
         .attr("cx", (d) => xScale(d.Month))
         .attr("cy", (d) => yScale(d.Price))
-        .attr("r", 0) // Reduce the radius to make the points smaller
+        .attr("r", 0) 
         .attr("fill", "steelblue");
   
-      // Update axes
+      
       svg.select(".x-axis").call(d3.axisBottom(xScale));
       svg.select(".y-axis").call(d3.axisLeft(yScale));
   
-      // Add the annotations
+    
       drawAnnotations(filteredData);
     }
   
-    // Function to filter data based on the selected range
+    
     function filterDataByRange(range) {
       if (range === "full-data") {
         return data;
@@ -237,7 +237,7 @@ function getFootnoteText(tab) {
       }
     }
   
-    // Event listener for button clicks
+
     const dataButtons = document.querySelectorAll(".data-btn");
     dataButtons.forEach(function (button) {
       button.addEventListener("click", function () {
@@ -254,7 +254,7 @@ function getFootnoteText(tab) {
           filteredData = { tab: "fullData", data: data };
         }
         if (selectedRange === "full-data") {
-            // If "Full Data" tab is selected, hide the footnote
+           
             svg.selectAll(".footnote").remove();
             updateLineplot(filteredData);
           } else {
@@ -263,7 +263,7 @@ function getFootnoteText(tab) {
       });
     });
   
-    // Initial line plot with first tab's data (1997-2006)
+    
     const firstTabData = { tab: "firstTab", data: filterDataByRange("1997-2006") };
     const secondTabData = { tab: "secondTab", data: filterDataByRange("2007-2015") };
     const thirdTabData = { tab: "thirdTab", data: filterDataByRange("2016-2022") };
